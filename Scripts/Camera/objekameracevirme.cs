23a63d71293fc0a29237b0325c07db137f54f1a6
Hakan <83926473+Vandora-41@users.noreply.github.com>
2022-05-21 03:33:21 +0300

    Camera
    Atadığımız şeyi direkt kameraya çeviriyor. 
    Hud gibi şeylerde kullandım.

---
+++ Scripts/Camera/objekameracevirme.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objekameracevirme : MonoBehaviour
+{
+    private Transform mainCameraTransform;
+
+    private void Start()
+    {
+        mainCameraTransform = Camera.main.transform;
+    }
+    private void LateUpdate()
+    {
+        transform.LookAt(
+            transform.position + mainCameraTransform.rotation * Vector3.forward,
+            mainCameraTransform.rotation * Vector3.up);
+    }
+}
+/*
+Hangi objeye atarsak kamera nereye hareket ederse etsin ona dönüyor.
+
+Ben health bar gibi şeylerde kullandım.
+-
+*/
